856ed1fb294bc88ee7567950fba35421eb2e993d
김동하 <dongha@gimdonghaui-Macmini.local>
2020-10-12 18:03:12 +0900

    Search Office update

--- pages/SearchOffice/SearchOfficeContainer.js
+++ pages/SearchOffice/SearchOfficeContainer.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchOfficePresenter from "./SearchOfficePresenter";
+import { postData } from "../../api";
 
-export default () => {
+const SearchOfficeContainer = () => {
+  const [officeList, setOfficeList] = useState([]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = async () => {
+    const form = new FormData();
+    form.append("method", "proc_office_list");
+    form.append("keys[0]", "of_sectors");
+    form.append("values[0]", "부동산");
+    form.append("likes[0]", "Y");
+    form.append("keys[1]", "of_address");
+    form.append("values[1]", "서울");
+    form.append("likes[1]", "Y");
+    form.append("of_address_lat", "129.039685");
+    form.append("of_address_lng", "35.160967");
+    form.append("offset", "10");
+    form.append("page", "1");
+
+    const { 
+      data: { list }
+    } = await postData(form);
+    console.log(list);
+    setOfficeList(list);
+  }
+
   return (
-    <SearchOfficePresenter/>
+    <SearchOfficePresenter
+      officeList={officeList}
+    />
   )
 }
+
+export default SearchOfficeContainer
--- pages/SearchOffice/SearchOfficePresenter.js
+++ pages/SearchOffice/SearchOfficePresenter.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default () => (
-  <div>SeacrhOffice</div>
+export default ({ officeList=[] }) => (
+  <div>
+    <div>
+      <div>
+        <input 
+          type="text" 
+          placeholder="원하시는 지역이나 지하철역을 입력해주세요."
+        />
+      </div>
+      <div>
+        <span>(지역/지하철역명) 주변 매물 총 {officeList.length}개</span>
+      </div>
+      <div>
+        {
+          officeList.map(office => (
+            <div key={office.of_idx}>
+              <span>{office.of_name}</span>
+              <span>{`월세 ${office.of_deposit}/${office.of_monthly}`}</span>
+              <span>{`${office.of_area}평`}</span>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+    <div>kakao map api</div>
+  </div>
 )
