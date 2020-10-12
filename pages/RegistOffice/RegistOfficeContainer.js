9cc9cd08e4b5e2fe01e69dffea92b3d7af119ecb
김동하 <dongha@gimdonghaui-Macmini.local>
2020-10-12 18:02:54 +0900

    Resist Office update

--- pages/RegistOffice/RegistOfficeContainer.js
+++ pages/RegistOffice/RegistOfficeContainer.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RegistOfficePresenter from "./RegistOfficePresenter";
 
 const RegistOfficeContainer = () => {
   const [registData, setRegistData] = useState({
+    token: "" ,
     companyName: "",
     president: "",
-    userName,
-    userEmail,
+    userName: "",
+    userEmail: "",
     phoneNum: "",
     certification: "", //휴대폰 인증 번호
     category: "", // 사무실 등록 구분
-    businessPaper: {}, // 사업자 등록증
+    businessPaper: [], // 사업자 등록증
+    zipcode: "",
     address1: "", 
     address2: "",
     officeNickname: "", 
@@ -25,22 +27,81 @@ const RegistOfficeContainer = () => {
     deposit: "", // 보증금
     monthly: "", // 월세
     managementFee: "", // 관리비
-    option: [],
-    keyword: [],
+    option: "",
+    keyword: "",
     description: "",
     pictures: []
   });
 
   const setState = (property, event) => {
+    if (property === "businessPaper" || property === "pictures") {
+      setRegistData({
+        ...registData,
+        [property]: event.target.files
+      });
+    } else {
+      setRegistData({
+        ...registData,
+        [property]: event.target.value
+      });
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const getUserInfo = async () => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    const userInfo = JSON.parse(localStorage.getItem("userInformation"));
     setRegistData({
       ...registData,
-      [property]: event.target.value
+      token,
+      userName: userInfo.name,
+      userEmail: userInfo.email
     });
+  }
+
+  const setHash = (property, event) => {
+    setRegistData({
+      ...registData,
+      [property]: registData[property] + event.target.value + "#"
+    });
+  }
+
+  const post = async () => {
+    console.log(registData);
+    const form = new FormData();
+    form.append("method", "proc_office_add");
+    form.append("mb_token", registData.token);
+    form.append("of_cate", registData.category);
+    form.append("of_work_auth", registData.businessPaper);
+    form.append("of_zip", registData.zipcode);
+    form.append("of_address", registData.address1);
+    form.append("of_address_detail", registData.address2);
+    form.append("of_name", registData.officeNickname);
+    form.append("of_sectors", registData.workStyle);
+    form.append("of_layer_all", registData.totalFloor);
+    form.append("of_layer_this", registData.myFloor);
+    form.append("of_area", registData.areaSize);
+    form.append("of_member_all", registData.headCount);
+    form.append("of_system", registData.leaseSystem);
+    form.append("of_member_want", registData.recruitCount);
+    form.append("of_work_ok", registData.businessRegist);
+    form.append("of_deposit", registData.deposit);
+    form.append("of_monthly", registData.monthly);
+    form.append("of_man_price", registData.managementFee);
+    form.append("of_option", registData.option);
+    form.append("of_keyword", registData.keyword);
+    form.append("of_img", registData.pictures);
   }
 
   return (
     <RegistOfficePresenter 
+      state={registData}
       setState={setState}
+      setHash={setHash}
+      post={post}
     />
   )
 }
--- pages/RegistOffice/RegistOfficePresenter.js
+++ pages/RegistOffice/RegistOfficePresenter.js
@@ -1,20 +1,128 @@
 import React from "react";
 import Link from "next/link";
 
-export default ({ setState, post }) => (
+const RegistOfficePresenter = ({ state, setState, setHash, post }) => (
   <div>
-    <Link>Home</Link>
+    <Link href="/">Home</Link>
     <p>
       <label>회사명</label> 
       <input type="text" onChange={event=>setState("companyName", event)} />
     </p>
     <p>
       <label>대표자 이름</label> 
-      <input type="text" onChange={event=>setState("companyName", event)} />
+      <input type="text" onChange={event=>setState("president", event)} />
     </p>
     <p>
-      <label>회사명</label> 
-      <input type="text" onChange={event=>setState("companyName", event)} />
+      <label>회원정보</label> 
+      <input type="text" disabled value={state.userName} />
+      <input type="text" disabled value={state.userEmail} />
+      <input type="text" placeholder="휴대폰번호" onChange={event=>setState("phoneNum", event)} />
+      <input type="button" value="인증번호 받기" />
+      <input type="text" placeholder="인증번호" onChange={event=>setState("certification", event)} />
+    </p>
+    <p>
+      <label>등록구분</label> 
+      <select value={state.category} onChange={event=>setState("category", event)}>
+        <option value="">등록 구분 선택</option>
+        <option value="month">월세</option>
+        <option value="day">1일</option>
+        <option value="freepass">프리패스</option>
+      </select>
+    </p>
+    <p>
+      <label>사업자등록증</label> 
+      <input 
+        type="file" 
+        accept="image/*"
+        multiple 
+        onChange={event=>setState("businessPaper", event)} 
+      />
+    </p>
+    <p>
+      <label>사무실 주소</label> 
+      <input type="text" onChange={event=>setState("zipcode", event)} />
+      <input type="text" onChange={event=>setState("address1", event)} />
+      <input type="text" onChange={event=>setState("address2", event)} />
+    </p>
+    <p>
+      <label>사무실 별명</label> 
+      <input type="text" onChange={event=>setState("officeNickname", event)} />
+    </p>
+    <p>
+      <label>업종 선택</label> 
+      <input type="text" onChange={event=>setState("workStyle", event)} />
+    </p>
+    <p>
+      <label>층수</label> 
+      <input type="text" onChange={event=>setState("totalFloor", event)} />
+      <input type="text" onChange={event=>setState("myFloor", event)} />
+    </p>
+    <p>
+      <label>면적</label> 
+      <input type="text" onChange={event=>setState("areaSize", event)} />
+    </p>
+    <p>
+      <label>전체인원</label> 
+      <input type="text" onChange={event=>setState("headCount", event)} />
+    </p>
+    <p>
+      <label>임대 방식</label> 
+      <select value={state.leaseSystem} onChange={event=>setState("leaseSystem", event)}>
+        <option value="">등록 구분 선택</option>
+        <option value="001">1일</option>
+        <option value="002">월세</option>
+        <option value="003">프리패스</option>
+      </select>
+    </p>
+    <p>
+      <label>모집인원</label> 
+      <input type="text" onChange={event=>setState("recruitCount", event)} />
+    </p>
+    <p>
+      <label>사업자등록</label> 
+      <select value={state.businessRegist} onChange={event=>setState("businessRegist", event)}>
+        <option value="">등록 가능 여부</option>
+        <option value="001">가능</option>
+        <option value="002">불가능</option>
+      </select>
+    </p>
+    <p>
+      <label>보증금</label> 
+      <input type="text" onChange={event=>setState("deposit", event)} />
+    </p>
+    <p>
+      <label>월세</label> 
+      <input type="text" onChange={event=>setState("monthly", event)} />
+    </p>
+    <p>
+      <label>관리비</label> 
+      <input type="text" onChange={event=>setState("managementFee", event)} />
+    </p>
+    <p>
+      <label>옵션</label> 
+      <input type="text" onChange={event=>setState("category", event)} />
+    </p>
+    <p>
+      <label>키워드</label> 
+      <input type="text" onChange={event=>setState("category", event)} />
+    </p>
+    <p>
+      <label>임대인 한마디</label> 
+      <input type="text" onChange={event=>setState("description", event)} />
+    </p>
+    <p>
+      <label>사진 등록</label> 
+      <input 
+        type="file" 
+        accept="image/*" 
+        multiple
+        onChange={event=>setState("pictures", event)} 
+      />
+    </p>
+    <p>
+      <button onClick={()=>post()}>사무실 등록 신청</button>
     </p>
   </div>
-)
+);
+
+export default RegistOfficePresenter;
